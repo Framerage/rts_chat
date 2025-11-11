ef0f1e70cb9ac4030a457522358bfc19ac25dbe1
Artem Igonin <officialigonin@mail.ru>
2025-11-11 04:08:05 +0400

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: "/rts_wss_chat",
+  },
+  base: "/rts_wss_chat",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+    modulePreload: false,
+    target: "esnext",
+    minify: true,
+    cssCodeSplit: false,
+  },
+});
