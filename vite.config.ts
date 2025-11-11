62ef525cd98b18c8b127b657f24ec443c71cabee
Artem Igonin <officialigonin@mail.ru>
2025-11-11 04:13:21 +0400

    correct microfront

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "chat_app",
+      filename: "chatEntry.js",
+      exposes: {
+        "./Chat": "./src/App",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
   server: {
     open: "/rts_chat",
   },
